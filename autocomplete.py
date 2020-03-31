c5c806fe382da385649b663362d8d6c28d9e83de
pawnlord <scruffduff321@gmail.com>
2020-03-31 14:36:54 -0400

    Added tab completion functionality Added the layout for multi-platform support Added more words makes it clear the screen added exception handling so the threads always stop

--- autocomplete.py
+++ autocomplete.py
@@ -3,29 +3,44 @@ from _thread import *
 import threading
 import time
 import threadclass
-import msvcrt
+import platform
 
 
 dictionary = []
 dictionary.append("")
 word = ""
 length_weight = 0.5
-
-def nonstoppinginput(word, prompt=""):
-    print(prompt + word, end='')
-    current = word
-    while True:
-        if msvcrt.kbhit():
-            chr = msvcrt.getche()
-            if ord(chr) == 13 or chr == '\n':
-                current = ""
-                break
-            elif ord(chr) >= 32:
-                current+= chr.decode('utf-8')
-                break
-    print ('')  # needed to move to next line
-    return current
-    
+if platform.platform()[:7] == "Windows":
+    import msvcrt
+    def nonstoppinginput(word, prompt, wordp):
+        os.system("cls")
+        print(prompt + word, end='\n')
+        current = word
+        while True:
+            if msvcrt.kbhit():
+                chr = msvcrt.getche()
+                if ord(chr) == 13 or chr == '\n':
+                    current = ""
+                    break
+                elif ord(chr) == 127:
+                    current = current[:-2]
+                    break
+                elif ord(chr) == 9:
+                    last_space = -1
+                    for i in range(len(current)):
+                        if current[i] == ' ':
+                            last_space = i
+                    current = current[:last_space+1]
+                    current += wordp[0]
+                    break
+                elif ord(chr) >= 32:
+                    current+= chr.decode('utf-8')
+                    break
+        print ('')  # needed to move to next line
+        return current
+else:
+    print("UNSUPPORTED PLATFORM!")
+    exit(1)
 
 with open("words.txt", "r") as ws:
     for c in ws.read():
@@ -46,7 +61,7 @@ def score_similarity(word1, word2):
         else:
             score -= 1-(c/min(len(word1), len(word2)))
     return score
-guessed_word = ""
+guessed_word = [""]
 word = [""]
 
 def autocomplete(running): 
@@ -68,17 +83,24 @@ def autocomplete(running):
             guess = ""
             guess = dictionary[i]
             current_best = score
-    if guess != guessed_word:
+    if guess != guessed_word[0]:
         print("GUESSED WORD: ", guess)
-    guessed_word = guess
+    guessed_word[0] = guess
     time.sleep(1)
 word[0] = ""
-autot = threadclass.basicthread(autocomplete)
-while True:
+try:
     autot = threadclass.basicthread(autocomplete, True)
-    if autot.running[0] == False:
-        autot.begin(False, name="AUTOCOMPLETE")
-    word[0] = nonstoppinginput(word[0], "YOUR WORD: ")
-    if word[0] == "EXITNOW":
-        break
-autot.running[0] = False
+    autot.begin(False, name="AUTOCOMPLETE")
+    while True:
+        word[0] = nonstoppinginput(word[0], "YOUR WORD: ", guessed_word)
+        if word[0] == "EXITNOW":
+            break
+    autot.running[0] = False
+except KeyboardInterrupt:
+    print("KeyboardInterrupt CAUGHT")
+    autot.running[0] = False
+    raise 
+except Exception as e:
+    print("EXCEPTION CAUGHT")
+    print(e)
+    autot.running[0] = False
